d6e09be87984c931c5866ed55c58b9c81e399006
Markus Peröbner <markus.peroebner@gmail.com>
2018-03-17 09:17:48 +0100

    makes query solln tag more complete

--- perkeep.spec.py
+++ perkeep.spec.py
@@ -3,6 +3,28 @@
 #
 import perkeep
 
-print(perkeep.query({
-    'expression': 'tag:solln'
-}))
+def main():
+    query_solln()
+
+def query_solln():
+    results = perkeep.query({
+        'expression': 'tag:solln',
+        'describe': {
+            'depth': 1,
+            'rules': [
+                {
+                    'attrs': [
+                        'camliContent',
+                    ],
+                },
+            ],
+        },
+    })
+    for blob in results['blobs']:
+        description = results['description']['meta'][blob['blob']]
+        if(not 'title' in description['permanode']['attr']):
+            continue
+        print(description['permanode']['attr']['title'][0])
+
+if __name__ == '__main__':
+    main()
