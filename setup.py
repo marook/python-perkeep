9f87cdbfc006c09954701e839715bb97309a9834
Markus Peröbner <markus.peroebner@gmail.com>
2018-03-18 10:17:54 +0100

    adds package installer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='perkeep',
+      version='0.1.0',
+      license='GPLv3',
+      description='a client library for perkeep',
+      author='Markus Peroebner',
+      author_email='markus.peroebner@gmail.com',
+      packages=['perkeep'],
+)
