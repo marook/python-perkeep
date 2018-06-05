e19a8f744bd51c1456d684690b8858e370a0e108
Markus Peröbner <markus.peroebner@gmail.com>
2018-06-05 13:12:24 +0200

    increases version to v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name='perkeep-utils',
-      version='0.1.0',
+      version='0.1.1',
       license='GPLv3',
       description='a client library for perkeep',
       author='Markus Peröbner',
